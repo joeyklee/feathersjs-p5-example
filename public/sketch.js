d9622426f691d938b9ea03a7cb62a626125bcea4
joeyklee <jsphknglee@gmail.com>
2019-02-12 00:08:42 -0500

    added working example

---
+++ public/sketch.js
@@ -0,0 +1,96 @@
+const client = feathers();
+
+// Connect to a different URL
+const restClient = feathers.rest('http://localhost:3030') // for dev
+// Configure an AJAX library (see below) with that client 
+client.configure(restClient.fetch(window.fetch));
+const todos = client.service('todos');
+
+let todoForm;
+let deleteContainer;
+
+let myData = [];
+
+function setup(){
+
+    createCanvas(400, 400).parent('#get-sketch-container');
+
+    todoForm = select("#todo-form");
+    todoForm.elt.addEventListener('submit', postData);
+
+    deleteContainer = select("#delete-items-container")
+
+    // initialize by getting the data in the database
+    getData();
+
+    createRemoveItems();
+    
+}
+
+function draw(){   
+    randomSeed(1);
+    background(200);
+    
+    myData.forEach( (item, idx) => {
+        text(item.todo,  random(width), random(height) );
+    })
+}
+
+async function deleteSelected(e){
+    try {
+        e.preventDefault();
+        console.log(e.currentTarget.id)
+        await todos.remove(e.currentTarget.id);
+        await getData()
+        await createRemoveItems();
+    }catch(err){
+        return err
+    }
+}
+
+async function createRemoveItems(){
+    try{
+        deleteContainer.elt.innerHTML = "";
+        const {data} = await todos.find()
+        data.forEach(item => {
+            // let removeBtn =`
+            //     <button onclick="${deleteSelected(item._id)}" data-id="${item._id}">REMOVE: ${item.todo}</button>
+            // `
+            let removeBtn = createElement('button', item.todo).parent('#delete-items-container');
+            removeBtn.id(item._id);
+            removeBtn.elt.addEventListener('click', deleteSelected);
+            // removeBtn.parent('#delete-items-container');
+        });
+        
+    } catch(err){
+        return err;
+    }
+}
+
+
+
+async function postData(e){
+    try {
+        e.preventDefault();    
+        const newForm = new FormData(e.currentTarget)
+        const newTodo = newForm.get('todo');
+
+        await todos.create({todo: newTodo})
+        await getData()
+        await createRemoveItems()
+    }catch(err){
+        return err
+    }
+}
+
+
+async function getData(){
+    try {
+        const retrievedData = await todos.find()
+        console.log('my new data', retrievedData);
+        myData = retrievedData.data;
+    }catch(err){
+        return err;
+    }
+}
+
---
+++ src/models/todos.model.js
@@ -0,0 +1,15 @@
+// todos-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const todos = new Schema({
+    todo: { type: String, required: true }
+  }, {
+    timestamps: true
+  });
+
+  return mongooseClient.model('todos', todos);
+};
